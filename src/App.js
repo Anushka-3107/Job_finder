08f6334c17acc92392c7f17bf94d52c52825daee
Nishant Chauhan <nishantpssn720@gmail.com>
2024-05-03 18:48:38 +0530

    filter completed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import JobCard from '../src/components/JobCard'
-import Filter from '../src/components/Filters/Filter'
+import Filter from './components/Filter'
 
 const App = () => {
   return (
     <div>
     <h1 className='text-center text-2xl my-6'>Search Jobs</h1>
-    <Filter/>
+    <Filter />
     <JobCard />
     </div>
   )
